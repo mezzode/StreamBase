a2ed1a2e805fb7007592315e032c918f79f0751a
mezzode <toranos@gmail.com>
2019-11-08 10:11:43 -0800

    Begin working on client

--- client.cpp
+++ client.cpp
@@ -1,6 +1,8 @@
 ﻿// client.cpp : Defines the entry point for the application.
 //
 
+#include <windows.h>
+#include "common.h"
 #include "client.h"
 
 using namespace std;
@@ -8,5 +10,15 @@ using namespace std;
 int main()
 {
 	cout << "I am the client." << endl;
+
+	// either open pipe once and reuse handle by doing CreateFile then TransactNamedPipe
+	// or open and close pipe for each message by using CallNamedPipe
+
+	// for now using CallNamedPipe for simplicity
+
+	// set buffer size depending on size of data we're sending?
+
+	const BOOL success = CallNamedPipe(pipeName, /* TODO */ );
+
 	return 0;
 }
