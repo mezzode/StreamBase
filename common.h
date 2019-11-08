5bf0c92ec0bee5767235ff19c8e3973d64c993d3
mezzode <toranos@gmail.com>
2019-11-08 09:51:55 -0800

    Add pipe server creation

---
+++ common.h
@@ -0,0 +1,1 @@
+const LPCSTR pipeName{ "\\.\pipe\StreamBase" };
--- server.cpp
+++ server.cpp
@@ -1,6 +1,8 @@
 ﻿// server.cpp : Defines the entry point for the application.
 //
 
+#include <windows.h>
+#include "common.h"
 #include "server.h"
 
 using namespace std;
@@ -8,5 +10,10 @@ using namespace std;
 int main()
 {
 	cout << "I am the server." << endl;
+
+	const DWORD pipeMode{ PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE };
+	// use PIPE_WAIT or PIPE_NOWAIT for sync/async?
+
+	const HANDLE handle = CreateNamedPipe(pipeName, PIPE_ACCESS_DUPLEX, pipeMode, PIPE_UNLIMITED_INSTANCES, 0, 0, 0, NULL);
 	return 0;
 }
