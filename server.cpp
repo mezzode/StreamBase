151f971248572cd070b2dee8cd4718a99d42517a
mezzode <toranos@gmail.com>
2019-11-08 09:19:22 -0800

    Add server executable

---
+++ server.cpp
@@ -0,0 +1,12 @@
+﻿// server.cpp : Defines the entry point for the application.
+//
+
+#include "server.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "I am the server." << endl;
+	return 0;
+}
